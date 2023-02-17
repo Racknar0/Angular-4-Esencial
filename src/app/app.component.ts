082963430cecbd18134f70d39d2011bd76e25125
Racknaro <racknarow1@gmail.com>
2023-02-16 21:54:47 -0500

    add iterations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,29 +4,33 @@ import { Libro } from './common/types';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Proyecto';
 
-  miVar:number = 0;
+  miVar: number = 0;
   miArr = [];
   miLibro: Libro;
-  color:string = 'red';
-  mostrar:boolean = false;
-  mostrar2:boolean = false;
-  diasSemana:string = 'lunes';
-  colorStyle:string = '';
+  color: string = 'red';
+  mostrar: boolean = false;
+  mostrar2: boolean = false;
+  diasSemana: string = 'lunes';
+  colorStyle: string = '';
+  libros: Array<Object>;
 
-  constructor(){
+  constructor() {
     this.miLibro = {
       id: 1,
       titulo: 'El Quijote',
       autor: 'Cervantes',
-      descripcion: 'Un libro muy largo'
-    }
+      descripcion: 'Un libro muy largo',
+    };
+
+    this.libros = [ "El Quijote", "El Hobbit", "El Señor de los Anillos"];
   }
 
-  
-
+  mostrarAutor() {
+    alert(this.libros[0]);
+  }
 }
